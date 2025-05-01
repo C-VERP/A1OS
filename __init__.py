2e9b4bb9a729cae90ee5ff4184fe3f575c6e14b6
ChristopheVdd <167391537+C-VERP@users.noreply.github.com>
2025-05-01 18:27:59 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from . import bol_messages
+from . import res_config_settings
---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Bol.com Helpdesk Integratie",
+    "version": "1.0",
+    "summary": "Importeer bol.com klantvragen als helpdesk tickets",
+    "category": "Helpdesk",
+    "author": "ChatGPT",
+    "depends": ["helpdesk"],
+    "data": ["bol_settings_view.xml", "cron.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False
+}
